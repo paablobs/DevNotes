5dcff3af08e0d14adb5024d70b0dc48a571141df
Pablo Bessone <pablo.bessone@outlook.com>
2025-06-11 15:39:20 +0000

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import styles from "./App.module.scss";
 
 function App() {
-  return <div className={styles.wrapper}>hello vite</div>;
+  return <div className={styles.wrapper}>hello vite 2</div>;
 }
 
 export default App;
